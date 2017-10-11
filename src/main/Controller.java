73ae1035e490ce38ba903e7e59542472f1f603f6
Tater Talker <sharkyxd7@gmail.com>
2017-10-11 15:11:34 -0400

    initcommit

---
+++ src/main/Controller.java
@@ -0,0 +1,11 @@
+package main;
+
+import javafx.stage.Stage;
+
+public class Controller {
+    Stage stage;
+    void setStage(Stage stage){
+        this.stage=stage;
+    }
+    void init(){}
+    }
---
+++ src/main/Main.java
@@ -0,0 +1,33 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private Stage primaryStage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main-window.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root,800,600);
+
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Penis");
+        this.primaryStage.setScene(scene);
+        this.primaryStage.show();
+
+        Controller controller = loader.getController();
+//        controller.setStage(this.primaryStage);
+//        controller.init();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
