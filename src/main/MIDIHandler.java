5894532fc64f79ba4dc4288211c240b0a1506d0d
pinejh <pinejh95@gmail.com>
2017-10-20 18:18:43 -0400

    Update MIDIHandler.java

--- src/main/MIDIHandler.java
+++ src/main/MIDIHandler.java
@@ -1,4 +1,29 @@
 package main;
 
+import javax.sound.midi.*;
+
 public class MIDIHandler {
+
+    public static void main(String[] args) {
+        try{
+            Synthesizer midiSynth = MidiSystem.getSynthesizer();
+            midiSynth.open();
+
+            Instrument[] instr = midiSynth.getDefaultSoundbank().getInstruments();
+            MidiChannel[] mChannels = midiSynth.getChannels();
+
+            midiSynth.loadInstrument(instr[0]);
+
+            mChannels[0].noteOn(60, 200);
+            mChannels[0].noteOn(64, 100);
+            mChannels[0].noteOn(67, 100);
+            try { Thread.sleep(5000);
+            } catch( InterruptedException e ) { }
+            mChannels[0].noteOff(60);
+            mChannels[0].noteOff(64);
+            mChannels[0].noteOff(67);
+
+        } catch (MidiUnavailableException e) {}
+    }
+
 }
