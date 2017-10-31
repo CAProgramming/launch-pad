680f63bab26e2e4f0dc7b0588d3c58f71110cf5e
nammyc <nammy.char@gmail.com>
2017-10-31 16:55:27 -0400

    Every button works with mouse clicks now

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -17,10 +17,40 @@ public class Controller {
     private Scene scene;
     private MIDIHandler midi;
 
-    public enum Mode { MIDI, SAMPLE };
+    public enum Mode { MIDI, SAMPLE }
 
     @FXML
     MidiCtrlButton _0_0;
+    @FXML
+    MidiCtrlButton _0_1;
+    @FXML
+    MidiCtrlButton _0_2;
+    @FXML
+    MidiCtrlButton _0_3;
+    @FXML
+    MidiCtrlButton _1_0;
+    @FXML
+    MidiCtrlButton _1_1;
+    @FXML
+    MidiCtrlButton _1_2;
+    @FXML
+    MidiCtrlButton _1_3;
+    @FXML
+    MidiCtrlButton _2_0;
+    @FXML
+    MidiCtrlButton _2_1;
+    @FXML
+    MidiCtrlButton _2_2;
+    @FXML
+    MidiCtrlButton _2_3;
+    @FXML
+    MidiCtrlButton _3_0;
+    @FXML
+    MidiCtrlButton _3_1;
+    @FXML
+    MidiCtrlButton _3_2;
+    @FXML
+    MidiCtrlButton _3_3;
 
 
     @FXML
@@ -30,9 +60,191 @@ public class Controller {
     }
 
     @FXML
+    void fire_0_1() {
+        _0_1.arm();
+        System.out.println("0, 1 fired");
+    }
+
+    @FXML
+    void fire_0_2() {
+        _0_2.arm();
+        System.out.println("0, 2 fired");
+    }
+
+    @FXML
+    void fire_0_3() {
+        _0_3.arm();
+        System.out.println("0, 3 fired");
+    }
+
+    @FXML
+    void fire_1_0() {
+        _1_0.arm();
+        System.out.println("1, 0 fired");
+    }
+
+    @FXML
+    void fire_1_1() {
+        _1_1.arm();
+        System.out.println("1, 1 fired");
+    }
+
+    @FXML
+    void fire_1_2() {
+        _1_2.arm();
+        System.out.println("1, 2 fired");
+    }
+
+    @FXML
+    void fire_1_3() {
+        _1_3.arm();
+        System.out.println("1, 3 fired");
+    }
+
+    @FXML
+    void fire_2_0() {
+        _2_0.arm();
+        System.out.println("2, 0 fired");
+    }
+
+    @FXML
+    void fire_2_1() {
+        _2_1.arm();
+        System.out.println("2, 1 fired");
+    }
+
+    @FXML
+    void fire_2_2() {
+        _2_2.arm();
+        System.out.println("2, 2 fired");
+    }
+
+    @FXML
+    void fire_2_3() {
+        _2_3.arm();
+        System.out.println("2, 3 fired");
+    }
+
+    @FXML
+    void fire_3_0() {
+        _3_0.arm();
+        System.out.println("3, 0 fired");
+    }
+
+    @FXML
+    void fire_3_1() {
+        _3_1.arm();
+        System.out.println("3, 1 fired");
+    }
+
+    @FXML
+    void fire_3_2() {
+        _3_2.arm();
+        System.out.println("3, 2 fired");
+    }
+
+    @FXML
+    void fire_3_3() {
+        _3_3.arm();
+        System.out.println("3, 3 fired");
+    }
+
+
+
+    @FXML
     void ceasefire_0_0() {
         _0_0.disArm();
         System.out.println("0, 0 ceased fire");
+    }
+
+    @FXML
+    void ceasefire_0_1() {
+        _0_1.disArm();
+        System.out.println("0, 1 ceased fire");
+    }
+
+    @FXML
+    void ceasefire_0_2() {
+        _0_2.disArm();
+        System.out.println("0, 2 ceased fire");
+    }
+
+    @FXML
+    void ceasefire_0_3() {
+        _0_3.disArm();
+        System.out.println("0, 3 ceased fire");
+    }
+
+    @FXML
+    void ceasefire_1_0() {
+        _1_0.disArm();
+        System.out.println("1, 0 ceased fire");
+    }
+
+    @FXML
+    void ceasefire_1_1() {
+        _1_1.disArm();
+        System.out.println("1, 1 ceased fire");
+    }
+
+    @FXML
+    void ceasefire_1_2() {
+        _1_2.disArm();
+        System.out.println("1, 2 ceased fire");
+    }
+
+    @FXML
+    void ceasefire_1_3() {
+        _1_3.disArm();
+        System.out.println("1, 3 ceased fire");
+    }
+
+    @FXML
+    void ceasefire_2_0() {
+        _2_0.disArm();
+        System.out.println("2, 0 ceased fire");
+    }
+
+    @FXML
+    void ceasefire_2_1() {
+        _2_1.disArm();
+        System.out.println("2, 1 ceased fire");
+    }
+
+    @FXML
+    void ceasefire_2_2() {
+        _2_2.disArm();
+        System.out.println("2, 2 ceased fire");
+    }
+
+    @FXML
+    void ceasefire_2_3() {
+        _2_3.disArm();
+        System.out.println("2, 3 ceased fire");
+    }
+
+    @FXML
+    void ceasefire_3_0() {
+        _3_0.disArm();
+        System.out.println("3, 0 ceased fire");
+    }
+
+    @FXML
+    void ceasefire_3_1() {
+        _3_1.disArm();
+        System.out.println("3, 1 ceased fire");
+    }
+
+    @FXML
+    void ceasefire_3_2() {
+        _3_2.disArm();
+        System.out.println("3, 2 ceased fire");
+    }
+
+    @FXML
+    void ceasefire_3_3() {
+        _3_3.disArm();
+        System.out.println("3, 3 ceased fire");
     }
 
     void init(Node root, Scene scene, Stage stage, MIDIHandler midi) {
@@ -43,6 +255,24 @@ public class Controller {
        
         createEventListeners(scene);
         _0_0.init(midi, 60, 155);
+        _0_1.init(midi, 61, 155);
+        _0_2.init(midi, 62, 155);
+        _0_3.init(midi, 63, 155);
+
+        _1_0.init(midi, 64, 155);
+        _1_1.init(midi, 65, 155);
+        _1_2.init(midi, 66, 155);
+        _1_3.init(midi, 67, 155);
+
+        _2_0.init(midi, 68, 155);
+        _2_1.init(midi, 69, 155);
+        _2_2.init(midi, 70, 155);
+        _2_3.init(midi, 71, 155);
+
+        _3_0.init(midi, 72, 155);
+        _3_1.init(midi, 73, 155);
+        _3_2.init(midi, 74, 155);
+        _3_3.init(midi, 75, 155);
         System.out.println("controller initialized");
     }
 
