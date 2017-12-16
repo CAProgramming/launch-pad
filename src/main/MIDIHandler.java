7cfa7c96489bcf2f8a82623ac31c2c3b1ad56d2d
nammyc <john.namgung@concordacademy.org>
2017-12-15 19:54:40 -0500

    Finished basic PlayableButton template, refactored MIDIHandler with Handler template

--- src/main/MIDIHandler.java
+++ src/main/MIDIHandler.java
@@ -1,14 +1,18 @@
 package main;
 
 import javax.sound.midi.*;
+import java.util.Map;
+import java.lang.Integer;
 
-public class MIDIHandler{
+
+public class MIDIHandler implements Handler<Integer>{
 
     Synthesizer midiSynth;
     MidiChannel[] mChannels;
 
 
     public MIDIHandler() {
+        super();
         try{
             midiSynth = MidiSystem.getSynthesizer();
             midiSynth.open();
@@ -17,12 +21,23 @@ public class MIDIHandler{
         } catch (MidiUnavailableException e) {}
     }
 
-    void noteOn(int noteNumber, int velocity, int instrument) {
-        midiSynth.getChannels()[0].programChange(instrument);
-        mChannels[0].noteOn(noteNumber, velocity);
+    @Override
+    public void play(Map<String, Integer> param) {
+        if(!param.isEmpty() && param.size() == 3) {
+            midiSynth.getChannels()[0].programChange(param.get("instrument"));
+            mChannels[0].noteOn(param.get("noteNum"), param.get("velocity"));
+        } else {
+            System.out.println("Parameters were not usable.");
+        }
     }
-    void noteOff(int noteNumber) {
-        mChannels[0].noteOff(noteNumber);
+
+    @Override
+    public void stop(Map<String, Integer> param) {
+        if(!param.isEmpty() && param.size() == 1) {
+            mChannels[0].noteOff(param.get("noteNum"));
+        } else {
+            System.out.println("Parameters were not usable.");
+        }
     }
 
 }
--- src/main/PlayableButton.java
+++ src/main/PlayableButton.java
@@ -1,43 +1,137 @@
 package main;
 
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+import java.util.Map;
+import java.util.HashMap;
 
 public class PlayableButton extends Button {
     private boolean midi;
-    private Button map;
-    private short shortParam1;
-    private short shortParam2;
-    private short shortParam3;
-    private String stringParam1;
-    private String stringParam2;
-    private String stringParam3;
-
+    private Handler handler;
     private boolean isOn;
 
-    // Constructors
+    // MIDI fields
+    private short noteNum;
+    private short instrument;
+    private short velocity;
 
-    public PlayableButton(boolean midi, String stringParam1) {
+
+    // ************************************* MIDI constructors
+    public PlayableButton(Handler handler, short noteNum) {
         super();
-        this.midi = midi;
-        this.stringParam1 = stringParam1;
+        if (handler.getClass().equals(MIDIHandler.class)) {
+            this.handler = handler;
+            this.midi = true;
+            this.isOn = false;
+            this.noteNum = noteNum;
+            this.instrument = 0;
+
+            this.velocity = 220;
+
+            this.setOnMousePressed(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    arm();
+                }
+            });
+
+            this.setOnMouseReleased(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    disArm();
+                }
+            });
+        } else {
+            System.out.println("You passed the wrong parameters for a " + handler.getClass() + "!");
+            this.handler = null;
+        }
     }
 
-    public PlayableButton(boolean midi, String stringParam1, String stringParam2, String stringParam3) {
+    public PlayableButton(Handler handler, short noteNum, short instrument) {
         super();
+        if (handler.getClass().equals(MIDIHandler.class)) {
+            this.handler = handler;
+            this.midi = true;
+            this.isOn = false;
+            this.noteNum = noteNum;
+            this.instrument = instrument;
+
+            this.velocity = 220;
+
+            this.setOnMousePressed(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    arm();
+                }
+            });
+
+            this.setOnMouseReleased(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    disArm();
+                }
+            });
+        } else {
+            System.out.println("You passed the wrong parameters for a " + handler.getClass() + "!");
+            this.handler = null;
+        }
+    }
+    // *************************************
+
+
+    // ************************************* Audio sample constructors
+    public PlayableButton(Handler handler, String samplePath) {
+        super();
+        if (handler.getClass().equals(SampleHandler.class)) {
+
+        } else {
+            System.out.println("You passed the wrong parameters for a " + handler.getClass() + "!");
+            this.handler = null;
+        }
+    }
+    // *************************************
+
+    // MIDI helper functions
+    public void setVelocity(short velocity) {
+        this.velocity = velocity;
     }
 
-    public PlayableButton(boolean midi, short shortParam1, short shortParam2, short shortParam3) {
-        super();
-    }
+    // Audio sample helper functions
 
     public void arm() {
-        if(this.midi == true) {
-            MIDIHandler midi = new MIDIHandler();
-        }
+        if (!isOn) {
+            super.arm();
+            super.fire();
+            getStyleClass().add("armed");
+            this.isOn = true;
 
+            if (this.midi == true) {
+                Map<String, Short> params = new HashMap<>();
+                params.put("noteNum", this.noteNum);
+                params.put("instrument", this.instrument);
+                params.put("velocity", this.velocity);
+                handler.play(params);
+            } else {
+            }
+
+            System.out.println("armed");
+        }
     }
 
     public void disArm() {
+        super.disarm();
+        getStyleClass().remove("armed");
+        this.isOn = false;
 
+        if (this.midi == true) {
+            Map<String, Short> params = new HashMap<>();
+            params.put("noteNum", this.noteNum);
+            handler.stop(params);
+        } else {
+        }
+
+        System.out.println("disarmed");
     }
 }
